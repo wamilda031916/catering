a3940b35cf05322265e7a73db8040ccce15fc139
Jean Wamilda <36716671+wamilda031916@users.noreply.github.com>
2019-01-21 20:21:15 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>AJ's Catering</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="bootstrap/bootstrap/css/main.css">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <style>
+	body{
+	background-image: url(images/index.jpg);
+	background-size: cover;
+	background-repeat: no-repeat;
+	width: 100%;
+	}
+</style>
+  <body>
+
+    <header>
+      <a href="#home" class="logo" data-scroll>AJ's CATERING</a>
+      <nav class="nav-collapse">
+        <ul>
+          <li class="menu-item active"><a href="#index.php" data-scroll>Home</a></li>
+          <li class="menu-item"><a href="#customer.php" data-scroll>Customer</a></li>
+          <li class="menu-item"><a href="#product.php" data-scroll>Products</a></li>
+          <li class="menu-item"><a href="#sales.php" data-scroll>Sales Invoice</a></li>
+		  <li class="menu-item"><a href="#salesitem.php" data-scroll>Sales Items</a></li>
+        </ul>
+      </nav>
+    </header>
+	
+  </body>
+</html>

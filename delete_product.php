bfd93d22df8c6a2f83838895e802d8cfe2200d5b
Jean Wamilda <36716671+wamilda031916@users.noreply.github.com>
2019-03-04 22:21:50 +0800

    delete file for product

--- delete_product.php
+++ delete_product.php
@@ -2,13 +2,13 @@
 
 include "connect.php";
 
-$id = $_GET['id'];
+$id = $_GET['product_code'];
 
 $sql = "DELETE FROM product WHERE product_code = '$id'";
 
-$res = mysqli_query($link, $sql);
+$res = mysqli_query($conn, $sql);
 
-mysqli_close($link);
+mysqli_close($conn);
 
-header('location: index.php');
+header('location: product.php');
 exit;

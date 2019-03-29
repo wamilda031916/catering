2f459f0f714c19a90f30757f1669865326271f1b
Jean Wamilda <36716671+wamilda031916@users.noreply.github.com>
2019-03-29 19:00:49 +0800

    insert file of sales invoice

--- insert_salesinvoice.php
+++ insert_salesinvoice.php
@@ -1,44 +1,42 @@
-<?php
-//insert.php;
+<?php include("connect.php");
 
-if(isset($_POST["submit"]))
-{
-  $servername = "localhost";
-  $dbUsername = "root";
-  $dbPassword = "";
-  $dbName = "catering";
- $conn = new PDO("mysql:host:localhost;dbname=catering","root","");
- $order_id = uniqid();
 $customer = $_POST['customer'];
-$quantity = $_POST['quantity'];
-$date = $_POST['date'];
 $terms = $_POST['terms'];
+$product = $_POST['products'];
+$qty = $_POST['quantity'];
+$unit = $_POST['unit'];
+$price = $_POST['price'];
+$date = $_POST['date'];
+$sql = "";
+$sales = array();
 
- $sql = "INSERT INTO sales_invoice(sales_number,customer_id,date,terms) VALUES ($order,$customer,$date1,$terms)";
- $conn->prepare($sql)->execute([':order' => '$order_id',':customer' => $customer, ':date1' => '$date', ':terms' => '$terms']);
-echo $sql;
+$insert = "INSERT INTO sales_invoice(customer_id,date,terms) VALUES($customer,'$date','$terms')";
+$res = mysqli_query($conn, $insert);
 
- for($count = 0; $count < count($_POST["customer"]); $count++)
- {  
-  $query = "INSERT INTO sales_item 
-  (sales_number,product_code, quantity,unit,unit_price) 
-  VALUES (:order_id, :product_code, :quantity, :unit, :unit_price)
-  ";
-  $statement = $conn->prepare($query);
-  $statement->execute(
-   array(
-    ':order_id' => $order_id,
-    ':product_code'  => $_POST["product"][$count], 
-    ':quantity' => $_POST["quantity"][$count], 
-    ':unit'  => $_POST["unit"][$count],
-    ':unit_price'  => $_POST["price"][$count],
-   )
-  );
- }
- $res = $statement->fetchAll();
- if(isset($res))
- {
-  echo 'ok';
- }
+
+$select = "SELECT sales_number FROM sales_invoice ORDER BY sales_number DESC LIMIT 1";
+$res1 = mysqli_query($conn, $select);
+$row = mysqli_fetch_array($res1);
+
+for ($num = 0; $num < count($_POST['products']); $num++){
+  $sales[]=$row['sales_number'];
 }
-?>
+
+
+$i = 0;
+while($i < count($_POST['products'])){
+  $product1 = mysqli_real_escape_string($conn,$product[$i]);
+  $qty1 = mysqli_real_escape_string($conn,$qty[$i]);
+  $unit1 = mysqli_real_escape_string($conn,$unit[$i]);
+  $price1 = mysqli_real_escape_string($conn,$price[$i]);
+  $sales1 = mysqli_real_escape_string($conn,$sales[$i]);
+  
+  $sql = "INSERT INTO sales_item(sales_number,product_code,quantity,unit,unit_price) VALUES($sales1,$product1,$qty1,'$unit1',$price1)";
+  mysqli_query($conn,$sql);
+  $i++;
+}
+echo $sql;
+if($sql == true){
+  echo '<script>window.location.href="sales_invoice.php";</script>';
+}
+  

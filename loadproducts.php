165d5a9a4c3d16965d93c3d001232ffb811c396a
Jean Wamilda <36716671+wamilda031916@users.noreply.github.com>
2019-04-01 13:38:28 +0800

    Delete loadproducts.php

--- loadproducts.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('connect.php');
-
-		$name = mysqli_real_escape_string($conn,$_POST['products']);
-		$show 	= "SELECT * FROM product WHERE product_code='$name' ";
-		$query 	= mysqli_query($conn,$show);
-
-	$row = json_encode(mysqli_fetch_assoc($query));
-	echo $row;
